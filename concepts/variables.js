6a3201a3530a4f4563dfac3429b85447d23a8343
Amaury.MD <mdamaury10@gmail.com>
2024-09-12 11:15:37 -0600

    02 variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,20 @@
+//Variables en JavaScript
+//Declaramos variables con let o const
+//let para variables que cambiarán de valor
+//const para variables que no cambiarán de valor
+let name;
+console.log(name);
+name="Peter";
+console.log(name);
+console.log(typeof name);
+name=18;
+console.log(name);
+console.log(typeof name);
+const PI = 3.1416;
+console.log(PI);
+console.log(typeof PI);
+// PI=3.1415;//Error
+console.log(sumar(10,20));
+const sumar(a,b){
+    return a+b
+}
