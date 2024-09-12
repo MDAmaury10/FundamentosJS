5a2d8fa996297d9d373da9ec178a98c2907b2f96
Amaury.MD <mdamaury10@gmail.com>
2024-09-12 11:30:19 -0600

    03 operadores

---
+++ concepts/operators.js
@@ -0,0 +1,39 @@
+//Operadores matematicos
+// + | -
+// * | /
+console.log(10+3)
+console.log(10-3)
+console.log(10/3)
+console.log(10%3)
+console.log(10**3)
+
+//operadores string
+//concatenacion con + 
+console.log("Hola"+""+"Mundo");
+
+//operadores de asignación
+let a=1;
+a +=3;  //a =a + 3
+a -=3;
+a *=3;
+a /=3;
+a %=3;
+a **=3;
+console.log(a);
+
+//operadores de comparación
+console.log(10>3);
+console.log(10<3);
+console.log(10>3);
+console.log(10>=3);
+console.log(10<=3);
+console.log(1==3);
+console.log(10===3);
+console.log(10!=3);
+console.log(10!==3);
+
+
+//operadores logicos
+console.log(true&&true) //and
+console.log(true||true)  //or
+console.log(!true)  //not
